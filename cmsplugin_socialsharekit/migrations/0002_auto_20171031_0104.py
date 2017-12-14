8129e49e6f0ab6cce789b87fb9f469281ba021bc
Mathieu Meylan <m.meylan@gmail.com>
2017-12-14 22:56:52 +0100

    add missing migration

---
+++ cmsplugin_socialsharekit/migrations/0002_auto_20171031_0104.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cmsplugin_socialsharekit', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='socialbutton',
+            name='button',
+            field=models.CharField(default=None, null=True, verbose_name='button', blank=True, max_length=20, choices=[('facebook', 'Facebook'), ('twitter', 'Twitter'), ('google-plus', 'Google +'), ('pinterest', 'Pinterest'), ('tumblr', 'Tumblr'), ('linkedin', 'Linkedin'), ('vk', 'VK'), ('buffer', 'Buffer'), ('email', 'Email')]),
+        ),
+    ]
